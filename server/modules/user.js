d831ab7d8466bebd6a4f786b3ca58a394ed70ded
tobias <tobiashubelpa@gmail.com>
2023-11-01 21:39:38 -0300

    inicio de modelado de los usuarios,comentarios y publicacions

---
+++ server/modules/user.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+const schema = mongoose.Schema;
+
+const userSchema = new schema({
+  nickname: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  lastname: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
